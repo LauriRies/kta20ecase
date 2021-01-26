e6f90e071bcbd42a48097d18928877d3f8c1c2fb
Lauri Ries <lauri.ries@tptlive.ee>
2021-01-26 23:55:32 +0200

    add modules

---
+++ webdev/main.js
@@ -0,0 +1,10 @@
+console.log('Hello node');
+const module = require('./module.js');
+module.hello();
+module.goodbye();
+
+import common from './module.js';
+common.hello();
+common.goodbye();
+let num = 1;
+num = "hello";
